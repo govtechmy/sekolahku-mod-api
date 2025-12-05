b23887f15a8d1167135c0821320806ac8d79fa0f
Zaid Aiman <zaidaiman@gmail.com>
2025-12-05 16:23:33 +0800

    minor update on server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,39 @@ import { registerApiRoutes } from './routes/index.route'
 
 async function buildServer(): Promise<FastifyInstance> {
   //build the server
-  const { isProduction } = config.env
-  const app = Fastify({ logger: config.loggerOptions(isProduction) }).withTypeProvider<ZodTypeProvider>()
+  const { isProduction, LOG_LEVEL } = config.env
+  const app = Fastify({
+    disableRequestLogging: true, // Disable default request logging
+    logger: {
+      level: LOG_LEVEL ?? 'info',
+      stream: {
+        write: chunk => {
+          try {
+            const obj = JSON.parse(chunk)
+            if (obj.method === 'GET' && obj.url === '/' && obj.body === '[omitted]') {
+              return // skip logging for health checks
+            }
+
+            const path = obj.method && obj.url ? `${obj.method} ${obj.url}` : undefined
+            const reordered = {
+              msg: obj.msg,
+              path: path,
+              params: obj.params,
+              body: obj.body,
+              reqId: obj.reqId,
+              level: obj.level,
+              time: obj.time,
+              pid: obj.pid,
+              hostname: obj.hostname,
+            }
+            process.stdout.write(`${JSON.stringify(reordered)}\n`)
+          } catch {
+            process.stdout.write(`${chunk}\n`) // fallback
+          }
+        },
+      },
+    },
+  }).withTypeProvider<ZodTypeProvider>()
 
   app.setValidatorCompiler(validatorCompiler)
   app.setSerializerCompiler(serializerCompiler)
