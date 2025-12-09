6eb5d5d92c185e119655400bf12e7c04992561e1
AMMAR HAIRI <145964801+ammarhairiHD@users.noreply.github.com>
2025-12-09 09:59:33 +0800

    rename all model require

--- src/controllers/schools.controller.ts
+++ src/controllers/schools.controller.ts
@@ -5,7 +5,7 @@ import { createErrorResponse, createSuccessResponse } from 'src/utils/response.u
 
 import type { CreateSchoolBody } from '@/schemas'
 
-import { EntitiSekolahModel } from 'src/models/school-entity.model'
+import { EntitiSekolahModel } from 'src/models/entiti-sekolah.model'
 // Zod now validates query parameters via `getNearbySchoolByLocationSchema` wired in the route
 
 export async function listSchools(req: FastifyRequest, reply: FastifyReply) {
