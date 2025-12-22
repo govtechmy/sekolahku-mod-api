55f17bf2ddb2bda896f78384e14578c215449630
PyConqueror <wanaqim@gmail.com>
2025-12-22 23:06:12 +0800

    SSD-620 - register code

--- src/plugins/index.plugin.ts
+++ src/plugins/index.plugin.ts
@@ -1,6 +1,7 @@
 import compress from '@fastify/compress'
 import type { FastifyInstance } from 'fastify'
 
+import { registerCentroidPlugin } from './centroid.plugin'
 import { registerEnvPlugin } from './env.plugin'
 import { registerRequestLogging } from './request-logging.plugin'
 import { registerSecurityPlugins } from './security.plugin'
@@ -8,6 +9,7 @@ import { registerSwaggerPlugins } from './swagger.plugin'
 
 export async function registerAllPlugins(app: FastifyInstance, isProduction: boolean): Promise<void> {
   await registerEnvPlugin(app)
+  await registerCentroidPlugin(app)
   await registerSecurityPlugins(app, isProduction)
   await registerSwaggerPlugins(app)
   await app.register(compress, { global: true })
